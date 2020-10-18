f1c1e8ca1f570f13461bed7e9bcaa6db92f71b80
Adina Milston <milston@g.jct.ac.il>
2020-10-18 17:58:32 +0300

    message

---
+++ Lesson1/Lesson1/Source.cpp
@@ -0,0 +1,18 @@
+/*
+* File : my first  program
+* Author: Adina Milston
+* date 18/10/2020
+*/
+
+#include <iostream>
+using namespace std;
+
+//comment
+
+int main()
+{
+	cout << " Chodesh Tov!!!" << endl;
+	cout << " you have 15 10 agorot\n"
+	
+	return 0;
+}
